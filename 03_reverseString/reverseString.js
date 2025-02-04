afcac3fe9bdba3ad54e8a60d8ee7fc0af2e1c706
Jabari <sundayafternoonchilling@gmail.com>
2025-02-04 09:01:00 -0500

    trying to figure out how to console log within node if possible

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,5 +2,12 @@ const reverseString = function() {
 
 };
 
+let string= "Hello";
+
+let breakUp = [...string]; 
+
+console.log(breakUp);
+
+
 // Do not edit below this line
 module.exports = reverseString;
