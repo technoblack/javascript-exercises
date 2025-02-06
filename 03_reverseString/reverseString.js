6c9efc0b64cddb505d8c4bc0b5ee6ab31efd6930
Jabari <sundayafternoonchilling@gmail.com>
2025-02-06 07:56:42 -0500

    clear up string. Remove hello

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,15 +1,32 @@
 const reverseString = function() {
 
+    let string= "Hello";
+
+    let breakUp = [...string]; 
+   
+    let backTogether = breakUp.reverse().toString(); 
+   
+    let finalString = backTogether.replace(/,/g,"");
+
+    return(finalString);
+
+
 };
 
-let string= "Hello";
-
-let breakUp = [...string]; 
-
-let backTogether = breakUp.reverse().toString();
 
 
-console.log(backTogether);
+
+ let string= "Hello";
+
+ let breakUp = [...string]; 
+
+ let backTogether = breakUp.reverse().toString(); 
+
+ let finalString = backTogether.replace(/,/g,"");
+
+
+
+ console.log(finalString);
 
 
 // Do not edit below this line
