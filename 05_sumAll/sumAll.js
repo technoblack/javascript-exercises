77a2d16c963e50274b3eefdaa7d754d566bf1ef9
Jabari <sundayafternoonchilling@gmail.com>
2025-02-17 06:00:50 -0500

    janky function right now

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+let finalSum = 0;
+
+for (let 1 = 0, i < val2, i++) {
+ 
+finalSum += val1, val2;
+
+return finalSum;
+
+}
 
 };
+
+console.log(finalSum)
 
 // Do not edit below this line
 module.exports = sumAll;
