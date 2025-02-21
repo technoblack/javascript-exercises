d3a91120b84201cbf0b8adc77f776a931098a3a4
Jabari <sundayafternoonchilling@gmail.com>
2025-02-21 09:26:06 -0500

    solved first four

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,25 +2,27 @@ const sumAll = function(val1, val2) {
 
 let finalSum = 0  
 
+if (val1 < 0 || val2 < 0) {
+ return "ERROR"
+} else if 
+
 for (let i = val1; i <= val2; i++) {
 finalSum +=i
 };
 
-return finalSum;
 
-// else if {
-
-for (let j = val2; i <= val1; j++)
+for (let j = val2; j <= val1; j++) {
 finalSum +=j    
+};
 
 return finalSum;
-// }
+
+
 
 };
 
 
-
-  console.log(sumAll(1,9))
+console.log(sumAll(-9,3))
 
 
 
