678453d3740eca1ff20d84eb9b4e01943f1399f8
Jabari <sundayafternoonchilling@gmail.com>
2025-02-22 09:26:07 -0500

    Got 4/7 in 05. started 06, reading documentation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,10 @@
-const sumAll = function(val1, val2) {
+/* const sumAll = function(val1, val2) {
 
 let finalSum = 0  
 
 if (val1 < 0 || val2 < 0) {
  return "ERROR"
-} else if 
+// } else if 
 
 for (let i = val1; i <= val2; i++) {
 finalSum +=i
@@ -17,13 +17,30 @@ finalSum +=j
 
 return finalSum;
 
-
-
-};
+ }; 
 
 
 console.log(sumAll(-9,3))
 
+Jabari until checking solution. Passed first 4 tests
+
+*/
+
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+};
 
 
 // Do not edit below this line
