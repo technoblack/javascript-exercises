4e1a8d6f6bd4d37fe11df6c31a2f707b6a8acd6c
Jabari <sundayafternoonchilling@gmail.com>
2025-02-25 07:25:50 -0500

    tryna figure out hoe to get function to account for years divisible by 4 and 400

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,18 @@
 const leapYears = function(year) {
 
-    let year = 0;
+  //  let year = 0; 
 
- if (year / 4 = 0 || year / 400 = 0)   
+ if (year % 4 == 0 && year % 400 == 0)  {
+    return true;
+ }
+
+ else {
+    return false;
+ }
 
 };
+
+console.log(leapYears(2004))
 
 // Do not edit below this line
 module.exports = leapYears;
