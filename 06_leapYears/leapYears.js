b923a39cf8891a21ff5329140a89e13c1838efa7
Jabari <sundayafternoonchilling@gmail.com>
2025-03-04 08:26:18 -0500

    Finished 06, on 07, not undertsnading Fahrenheit/Celsius conversion

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,8 @@
-const leapYears = function(year) {
+/* const leapYears = function(year) {
 
 // let year = 0; 
 
-/* for (i = 0; i < infiity; i+= 4) {
-   year +=i;
-}
-*/
+
  if (year % 4 == 0 && year % 400 == 0)  {
     return true;
  }
@@ -18,5 +15,25 @@ const leapYears = function(year) {
 
 console.log(leapYears(2004))
 
+Jabari code Got 4 out of 6
+
+*/
+
+const leapYears = function (year) {
+   const isYearDivisibleByFour = year % 4 === 0;
+   const isCentury = year % 100 === 0;
+   const isYearDivisibleByFourHundred = year % 400 === 0;
+ 
+   if (
+     isYearDivisibleByFour &&
+     (!isCentury || isYearDivisibleByFourHundred)
+   ) {
+     return true;
+   } else {
+     return false;
+   }
+ }
+
+console.log(leapYears(1996))
 // Do not edit below this line
 module.exports = leapYears;
