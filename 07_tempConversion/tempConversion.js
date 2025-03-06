0ae8f8e671b3c1097f3450977a7881889582c171
Jabari <sundayafternoonchilling@gmail.com>
2025-03-06 07:25:43 -0500

    tryna get forumla to work, how to delcare farenheit properly

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+
+  let farenheit = 0;
+
+let celcius = (farenheit - 32) / 1.8 
+
 };
+
+console.log(convertToCelsius(45))
 
 const convertToFahrenheit = function() {
 };
