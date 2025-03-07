29c7c09cf7502bc95e9982d3df3f77ad35f4024a
Jabari <sundayafternoonchilling@gmail.com>
2025-03-07 07:25:35 -0500

    forumla finished, testing next

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,23 @@
 const convertToCelsius = function(farenheit) {
 
-  let farenheit = 0;
 
-let celcius = (farenheit - 32) / 1.8 
+let celcius = (farenheit - 32) / 1.8; 
+
+return Math.round(celcius * 10)/10;
 
 };
 
-console.log(convertToCelsius(45))
+console.log(convertToCelsius(32))
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+
+let farenheit = (celcius * 1.8) + 32;
+
+return Math.round(farenheit * 10)/10;
+
 };
+
+console.log(convertToFahrenheit(0))
 
 // Do not edit below this line
 module.exports = {
